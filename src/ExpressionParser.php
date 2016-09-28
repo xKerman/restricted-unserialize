f71644f82f5f6e16463018f4e949fd4088252f11
xKhorasan <xKhorasan@gmail.com>
2016-09-29 08:33:58 +0900

    add ExpressionParser

---
+++ src/ExpressionParser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace xKerman\Restricted;
+
+class ExpressionParser implements ParserInterface {
+    public function parse(Source $source) {
+        $parser = $this->createParser($source);
+        list($result, $source) = $parser->parse($source);
+        return [$result, $source];
+    }
+
+    private function createParser($source) {
+        switch ($source->peek()) {
+        case 'N':
+            return new NullParser();
+        case 'b':
+            return new BooleanParser();
+        case 'i':
+            return new IntegerParser();
+        case 'd':
+            return new FloatParser();
+        case 's':
+            return new StringParser();
+        case 'a':
+            return new ArrayParser();
+        default:
+            $source->triggerError();
+        }
+    }
+}
--- src/function.php
+++ src/function.php
@@ -4,32 +4,7 @@ namespace xKerman\Restricted;
 
 function unserialize($str) {
     $source = new Source($str);
-    switch ($source->peek()) {
-    case 'N':
-        $parser = new NullParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    case 'b':
-        $parser = new BooleanParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    case 'i':
-        $parser = new IntegerParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    case 'd':
-        $parser = new FloatParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    case 's':
-        $parser = new StringParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    case 'a':
-        $parser = new ArrayParser();
-        list($result, $source) = $parser->parse($source);
-        return $result;
-    default:
-        return false;
-    }
+    $parser = new ExpressionParser();
+    list($result, $source) = $parser->parse($source);
+    return $result;
 }
