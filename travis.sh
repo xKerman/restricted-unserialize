b2612abf4a5034040dbd5473f8ade5d796b492b5
xKerman <xKhorasan@gmail.com>
2017-05-30 23:39:21 +0900

    set up xdebug for PHP 5.2
    use zend_extension_ts. see https://xdebug.org/docs/install
    > For PHP versions earlier than 5.3 and threaded usage of PHP (Apache 2 worker MPM or the ISAPI module), add: zend_extension_ts="/wherever/you/put/it/xdebug.so" instead.

--- travis.sh
+++ travis.sh
@@ -6,5 +6,8 @@ if [ $? -eq 0 ]; then
 else
     rm -rf src/ test/
     mv generated/* ./
+    pecl install xdebug-2.2.7
+    sed -i '/zend_extension_ts="xdebug.so"/d' ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    sed -i 's/zend_extension/zend_extension_ts/g' ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
     phpunit --bootstrap src/xKerman/Restricted/bootstrap.php
 fi
