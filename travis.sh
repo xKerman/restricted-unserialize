08383c0da65abe34d147194e029c606e48487fac
xKerman <xKhorasan@gmail.com>
2017-06-03 16:41:50 +0900

    separate phpunit config file for PHP5.2

--- travis.sh
+++ travis.sh
@@ -4,10 +4,8 @@ php -v | head -n1 | grep -q -vF 'PHP 5.2'
 if [ $? -eq 0 ]; then
     composer test
 else
-    rm -rf src/ test/
-    mv generated/* ./
     pecl install xdebug-2.2.7
     sed -i '/zend_extension_ts="xdebug.so"/d' ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     sed -i 's/zend_extension/zend_extension_ts/g' ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
-    phpunit --bootstrap src/xKerman/Restricted/bootstrap.php
+    phpunit --bootstrap generated/src/xKerman/Restricted/bootstrap.php --configuration phpunit.php52.xml
 fi
