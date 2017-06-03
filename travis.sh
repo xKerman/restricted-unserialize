467df27eef93ea4f1db389679f283078dddbf66d
xKerman <xKhorasan@gmail.com>
2017-06-04 07:44:04 +0900

    remove test script for travis-ci

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -
-
-php -v | head -n1 | grep -q -vF 'PHP 5.2'
-if [ $? -eq 0 ]; then
-    composer test
-else
-    phpunit --bootstrap generated/src/xKerman/Restricted/bootstrap.php --configuration phpunit.php52.xml
-fi
